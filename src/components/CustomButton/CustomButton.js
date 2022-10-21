297363ac1a9cdb9c01fd5cb8d67e2676c32b9725
Unicode-tasks <darshitsarda10@gmail.com>
2022-10-21 12:27:28 +0530

    Third commit

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,10 +1,18 @@
 import React from "react";
 import {View,Text,StyleSheet,Pressable} from 'react-native';
 
-const CustomButton = ({ onPress, text }) => {
+const CustomButton = ({ onPress, text, type, bgColor, fgColor}) => {
     return(
-        <Pressable onPress={onPress} style={styles.container}>
-            <Text style={styles.text}>{text}</Text>
+        <Pressable onPress={onPress} style={[
+            styles.container, 
+            styles['container_${type}'],
+            bgColor?{backgroundColor: bgColor}:{}
+            ]}>
+            <Text style={[
+                styles.text,
+                styles['text_${type}'],
+                fgColor?{color:fgColor}:{}
+                ]}>{text}</Text>
         </Pressable>
     )
 }
@@ -16,6 +24,9 @@ container: {
     borderRadius: 5,
     alignItems:'center',
     marginTop: 10,
+},
+container_TERTIARY:{
+backgroundColor:'#ff0000'
 },
 text:{
 fontWeight:'bold',
--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Image, StyleSheet, useWindowDimensions, TextInput } from 'react-native';
+import { View, Text, Image, StyleSheet, useWindowDimensions, TextInput, ScrollView} from 'react-native';
 import Logo from '../../../assets/images/Logo.png';
 import CustomInput from "../../components/CustomInput";
 import CustomButton from "../../components/CustomButton/CustomButton";
@@ -10,9 +10,21 @@ const SignInScreen = () => {
     const{height} = useWindowDimensions();
     const onSignInPressed = () =>{
         console.warn("Sign In");
-    }
+    };
+
+    const onForgotPasswordPressed = () =>{
+        console.warn('onForgotPasswordPressed'); 
+    };
+    const onSignInFacebook = () =>{
+console.warn('onSignInFacebook')
+    };
+    const onSignInGoogle = () =>{
+console.warn('onSignInGoogle')
+    };
     return (
+      <ScrollView>
       <View style={styles.root}>
+        <Text styles={styles.text}>The CookBook</Text>
         <Image 
         source={Logo} 
         style={[styles.Logo, {height: height*0.25}]} 
@@ -28,7 +40,22 @@ const SignInScreen = () => {
         secureTextEntry={true}
         />
         <CustomButton text="Sign In" onPress={onSignInPressed}/>
+        <CustomButton text="Forgot Password" onPress={onForgotPasswordPressed} type="TERTIARY"/>
+        
+        <CustomButton text="Sign In with Facebook" 
+        onPress={onSignInFacebook}
+        bgColor="#e7eaf4"
+        fgColor="#4765a9"
+        />
+        
+        <CustomButton text="Sign In with Google" 
+        onPress={onSignInGoogle}
+        bgColor="#f8eeec"
+        fgColor="#dd4d44"
+        />
+
       </View>
+      </ScrollView>
     )
   }
 
@@ -43,5 +70,8 @@ const styles = StyleSheet.create({
         maxWidth: 700,
         height: 250,
     },
+    text:{
+        fontSize: 40,
+    }
 });
   export default SignInScreen
